b1a4f73dd8553990b2cf7fd757a741941ca0606e
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-02-23 21:08:25 +0000

    Inject source content

--- index.js
+++ index.js
@@ -9,23 +9,26 @@ module.exports = function(/* no options */) {
     return mapEachResource(function(resource, supervisor) {
         var transpiledJs = resource.withType('javascript');
         var resourcePath = resource.path() && resource.path().absolute();
+        var coffeeData = resource.data();
         var options = {
             // TODO: support bare and literate explicitly
             // bare:          options.bare,
             // literate:      options.literate,
             sourceMap:     true,
             filename:      resourcePath,
-            sourceFiles:   [resource.filename()],
+            sourceFiles:   [resourcePath],
             generatedFile: transpiledJs.filename()
         };
 
         try {
-
-            var output = coffee.compile(resource.data(), options);
+            var output = coffee.compile(coffeeData, options);
             var jsData = output.js;
             var sourceMapData = output.v3SourceMap;
 
-            var sourceMap = SourceMap.fromMapData(sourceMapData);
+            var sourceMap = SourceMap.
+                fromMapData(sourceMapData).
+                // Not added by coffee.compile, but we like to have it
+                withSourceContent(resourcePath, coffeeData);
 
             // If the source had a sourcemap, rebase the CoffeeScript
             // sourcemap based on that original map
